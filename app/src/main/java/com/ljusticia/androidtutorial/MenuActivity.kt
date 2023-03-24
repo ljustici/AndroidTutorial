4b9b674edd43e68e4e38fb2e22e986e5da999947
l.justicia.ruiz <l.justicia.ruiz@accenture.com>
2023-03-24 16:37:45 +0100

    added weight, heigh, imc, result

--- app/src/main/java/com/ljusticia/androidtutorial/MenuActivity.kt
+++ app/src/main/java/com/ljusticia/androidtutorial/MenuActivity.kt
@@ -5,19 +5,29 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import com.ljusticia.androidtutorial.firstapp.FirstAppActivity
+import com.ljusticia.androidtutorial.imccalculator.ImcCalculatorActivity
 
 class MenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
 
+        //SALUDApp
         val btSaludapp = findViewById<Button>(R.id.btSaludapp)
-
         btSaludapp.setOnClickListener{ navigateToSaludapp()}
+
+        //IMC App
+        val btImcApp = findViewById<Button>(R.id.btIMCApp)
+        btImcApp.setOnClickListener{ navigateToImcApp()}
     }
 
     private fun navigateToSaludapp(){
         val intent = Intent(this, FirstAppActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun navigateToImcApp(){
+        val intent = Intent(this, ImcCalculatorActivity::class.java)
         startActivity(intent)
     }
 }
