7a05cb352acba3b479acae7b688e9cc922f0e428
l.justicia.ruiz <l.justicia.ruiz@accenture.com>
2023-03-24 09:11:22 +0100

    added button & textview

--- app/src/main/java/com/ljusticia/androidtutorial/firstapp/FirstAppActivity.kt
+++ app/src/main/java/com/ljusticia/androidtutorial/firstapp/FirstAppActivity.kt
@@ -2,12 +2,28 @@ package com.ljusticia.androidtutorial.firstapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.widget.AppCompatButton
+import androidx.appcompat.widget.AppCompatEditText
 import com.ljusticia.androidtutorial.R
 
 class FirstAppActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_first_app)
+        //botón
+        val btStart = findViewById<AppCompatButton>(R.id.btStart)
+
+        //EditText
+        val etName = findViewById<AppCompatEditText>(R.id.etName)
+
+        btStart.setOnClickListener {
+            Log.i("ljustici","Botón pulsado ${etName.text.toString()}") //Muestra mensaje en logcat
+
+        }
+
+
+
         //Al arrancar la pantalla
     }
 }
